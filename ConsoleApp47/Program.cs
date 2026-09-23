531c8bad51ff171c0e4e74ecaf4db85bb61f702f
Aykhan003 <aykhanrb@code.edu.az>
2026-09-23 21:35:12 +0400

    update code

--- ConsoleApp47/Program.cs
+++ ConsoleApp47/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿static async Task Main(string[] args)
+{
+    while (true)
+    {
+        Console.WriteLine("=== E-COMMERCE CONCURRENCY & DI ENGINE ===\r\n");
+        Console.WriteLine("1. Yeni Sifariş Əlavə Et (Async)");
+        Console.WriteLine("2. Dependency Injection İlə Bildiriş Göndərməni Test Et");
+        Console.WriteLine("3. Sifarişləri Paralel Emal Et (Race Condition & Lock Testi)\r\n");
+        Console.WriteLine("4. Sifarişləri JSON Faylına Saxla (Async Streams & Using)\r\n");
+        Console.WriteLine("5. Fayldan Sifarişləri Oxu(Deserialization)\r\n");
+        Console.WriteLine("6. Sifariş Obyektini Reflection İlə İnspeksiya Et\r\n");
+        Console.WriteLine("7. Record və Deep Copy Testi\r\n");
+        Console.WriteLine("0. Çıxış");
+    }
+}
